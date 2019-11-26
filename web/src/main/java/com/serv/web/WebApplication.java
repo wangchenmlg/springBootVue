b88335d84c64280e32d6b6513a0bad2b0702ff1f
chong.zi.work.note.book <wangchenmlg@163.com>
2019-11-26 17:17:45 +0800

    add the application xml file valid

--- web/src/main/java/com/serv/web/WebApplication.java
+++ web/src/main/java/com/serv/web/WebApplication.java
@@ -2,8 +2,10 @@ package com.serv.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+//@ImportResource("classpath:applicationContext.xml")
 public class WebApplication {
 
 	public static void main(String[] args) {
