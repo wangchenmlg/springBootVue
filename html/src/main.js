2a4cb19520c5445ad6e925472feef7eb51aadc83
chong.zi.work.note.book <wangchenmlg@163.com>
2019-11-28 13:54:56 +0800

    add the base proxy function!

--- html/src/main.js
+++ html/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+
+//将axios挂载在Vue扩展上,在其他地方使用只需使用 this.$http来代替axios;
+Vue.prototype.$http=axios
+//配置baseUrl
+axios.defaults.baseURL = '/api'
 
 Vue.config.productionTip = false
 
---
+++ html/vue.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  outputDir: 'dist',   //build输出目录
+  assetsDir: 'assets', //静态资源目录（js, css, img）
+  lintOnSave: false, //是否开启eslint
+  devServer: {
+    open: true, //自动弹窗
+    host: "localhost",
+    port: '8091',
+    https: false,
+    hotOnly: false,
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:8090', //API服务器的地址
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': '/test' //映射地址
+        }
+      }
+    }
+  }
+}
+
